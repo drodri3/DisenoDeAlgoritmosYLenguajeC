6a763b54f8202eba15c0e9df23b50520a4bc8716
Andrei Leon Salas <andreileonsalas@gmail.com>
2023-09-06 19:30:18 -0600

    agregar struct de libros y agregadas funciones

--- proyecto1/main.c
+++ proyecto1/main.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 #include <string.h>
+#include <time.h>
+
+
+struct Libro
+{
+    char titulo[100];
+    struct tm fechaPrestamo; // Campo para la fecha de préstamo
+    int estado; // 0 para disponible, 1 para prestado
+    int multa;
+};
 
 struct Usuario
 {
@@ -7,9 +17,12 @@ struct Usuario
     char nombre[50];
     char apellido[50];
     char contrasena[50];
-    int tieneMultas;
-    int tienePrestamo;
+    struct Libro libros[10]; // Cambiar el tamaño del array según sea necesario
+    int numLibros; // Número de libros prestados
 };
+
+int multaPorDia = 75;
+int multaPorExtravio = 500;
 
 void imprimirMenuPrincipal()
 {
@@ -19,19 +32,19 @@ void imprimirMenuPrincipal()
     printf("3. Salir\n");
 }
 
+void imprimirMenuMultas()
+{
+    printf("\nMenú de Multas\n");
+    printf("1. Cancelar multa\n");
+    printf("2. Salir\n");
+}
+
 void imprimirMenuUsuario()
 {
     printf("\nMenú de Opciones\n");
     printf("1. Solicitar un préstamo\n");
     printf("2. Devolver un libro\n");
     printf("3. Salir\n");
-}
-
-void imprimirMenuMultas()
-{
-    printf("\nMenú de Multas\n");
-    printf("1. Cancelar multa\n");
-    printf("2. Salir\n");
 }
 
 void registrarUsuario(struct Usuario usuarios[], int *numUsuarios)
@@ -45,8 +58,9 @@ void registrarUsuario(struct Usuario usuarios[], int *numUsuarios)
     scanf("%s", usuarios[*numUsuarios].apellido);
     printf("Contraseña: ");
     scanf("%s", usuarios[*numUsuarios].contrasena);
-    usuarios[*numUsuarios].tieneMultas = 0;
-    usuarios[*numUsuarios].tienePrestamo = 0;
+
+    // Inicializar el número de libros prestados a 0
+    usuarios[*numUsuarios].numLibros = 0;
 
     (*numUsuarios)++;
 }
@@ -76,33 +90,152 @@ int iniciarSesion(struct Usuario usuarios[], int numUsuarios)
 void solicitarPrestamo(struct Usuario *usuario)
 {
     printf("Función: Solicitar un préstamo para el usuario %s %s.\n", usuario->nombre, usuario->apellido);
+    //aca hay que meter la informacion del libro en el array de libros del usuario, agregar que fecha se presto y cambiar el estado del libro a prestado
+    
 }
 
 void devolverLibro(struct Usuario *usuario)
 {
     printf("Función: Devolver un libro para el usuario %s %s.\n", usuario->nombre, usuario->apellido);
+    //aca hay que meter todo lo de devolver un libro, tanto el costo de pagar por dias sin pagar, como el costo si se extravio
 }
 
-void menuMultas(struct Usuario *usuario)
+
+int calcularMultaPorDia(struct Libro *libro)
 {
-    printf("Función: menu multas para el usuario %s %s.\n", usuario->nombre, usuario->apellido);
+    int totalMulta = 0;
+    
+    // Obtener la fecha y hora actual
+    time_t tiempo = time(0);
+    struct tm *tlocal = localtime(&tiempo);
+
+    // Obtener el año actual completo, se agrega 1900 porque el año 0 es 1900
+    int anioActual = tlocal->tm_year + 1900;
+
+    // Calcular la diferencia en años, meses y días desde la fecha de préstamo hasta la fecha actual
+    int aniosDiferencia = anioActual - libro->fechaPrestamo.tm_year;
+    int mesesDiferencia = tlocal->tm_mon - libro->fechaPrestamo.tm_mon;
+    int diasDiferencia = tlocal->tm_mday - libro->fechaPrestamo.tm_mday;
+
+    // Calcular la multa total
+    if (aniosDiferencia > 0 || (aniosDiferencia == 0 && mesesDiferencia > 0) || (aniosDiferencia == 0 && mesesDiferencia == 0 && diasDiferencia > 0)) {
+        totalMulta = (aniosDiferencia * 365 + mesesDiferencia * 30 + diasDiferencia) * multaPorDia;
+    }
+
+    return totalMulta;
 }
+
+int calcularMultaPorExtravio(struct Libro *libro)
+{
+
+    return multaPorExtravio;
+}
+
+
 
 void cancelarMulta(struct Usuario *usuario)
 {
+
+    // Imprimir la lista de libros con multa
+    printf("Libros con multa:\n");
+    for (int i = 0; i < usuario->numLibros; i++)
+    {
+        if (usuario->libros[i].multa > 0)
+        {
+            printf("%d. %s\n", i + 1, usuario->libros[i].titulo);
+        }
+    }
+
+    // Solicitar el libro a cancelar la multa
+    int opcionLibro;
+    printf("Elija el libro a cancelar la multa: ");
+    scanf("%d", &opcionLibro);
+
+    // Obtener el libro seleccionado
+    struct Libro *libro = &usuario->libros[opcionLibro - 1];
+
+    // Calcular la multa total
+    int totalMulta = calcularMultaPorDia(libro);
+    
+    // Imprimir la multa total
+    printf("Multa total: %d\n", totalMulta);
+
+    // Cancelar la multa
+    libro->multa = 0;
+
+    // Imprimir mensaje de multa cancelada
     printf("Multa cancelada para el usuario %s %s.\n", usuario->nombre, usuario->apellido);
-    usuario->tieneMultas = 0;
+
+    
 }
 
-void reportarPerdida(struct Usuario *usuario)
+int tieneMultas(struct Usuario *usuario)
 {
-    printf("Pérdida reportada para el usuario %s %s.\n", usuario->nombre, usuario->apellido);
+    int tieneMultas = 0;
+    for (int i = 0; i < usuario->numLibros; i++)
+    {
+        if (usuario->libros[i].multa > 0)
+        {
+            tieneMultas = 1;
+            break;
+        }
+    }
+    return tieneMultas;
 }
+
+int tienePrestamo(struct Usuario *usuario)
+{
+    return (usuario->numLibros > 0);
+}
+
+
 
 int main()
 {
     struct Usuario usuarios[100];
     int numUsuarios = 0;
+
+    //imprimir dia actual
+
+    int dia, mes, anio;
+    time_t tiempo = time(0);
+    struct tm *tlocal = localtime(&tiempo);
+    dia = tlocal->tm_mday;
+    mes = tlocal->tm_mon + 1;
+    anio = tlocal->tm_year + 1900;
+
+    printf("Fecha actual: %d/%d/%d\n", dia, mes, anio);
+    
+    printf("Dia: %d\n", dia);
+    printf("Mes: %d\n", mes);
+    printf("Anio: %d\n", anio);
+
+    
+    // Agregar un usuario sin multas
+    strcpy(usuarios[numUsuarios].correo, "a");
+    strcpy(usuarios[numUsuarios].nombre, "Juan");
+    strcpy(usuarios[numUsuarios].apellido, "Pérez");
+    strcpy(usuarios[numUsuarios].contrasena, "a");
+    usuarios[numUsuarios].numLibros = 0; // Sin libros prestados
+    numUsuarios++;
+
+    // Agregar un usuario con multa
+    strcpy(usuarios[numUsuarios].correo, "b");
+    strcpy(usuarios[numUsuarios].nombre, "María");
+    strcpy(usuarios[numUsuarios].apellido, "Gómez");
+    strcpy(usuarios[numUsuarios].contrasena, "b");
+    usuarios[numUsuarios].numLibros = 1; // Un libro prestado
+    strcpy(usuarios[numUsuarios].libros[0].titulo, "Libro con multa");
+    usuarios[numUsuarios].libros[0].fechaPrestamo.tm_mday = 1; // Día de préstamo
+    usuarios[numUsuarios].libros[0].fechaPrestamo.tm_mon = 1; // Mes de préstamo
+    usuarios[numUsuarios].libros[0].fechaPrestamo.tm_year = 2020; // Año de préstamo
+
+    usuarios[numUsuarios].libros[0].estado = 1; // Libro prestado
+    usuarios[numUsuarios].libros[0].multa = 150; // Multa asociada al libro
+    numUsuarios++;
+
+    
+
 
     int opcion;
     do
@@ -123,7 +256,7 @@ int main()
             {
                 struct Usuario *usuarioActual = &usuarios[indiceUsuario];
 
-                if (usuarioActual->tieneMultas)
+                if (tieneMultas(usuarioActual))
                 {
                     printf("No puede acceder al menú de opciones debido a multas pendientes.\n");
                     int opcionMultas;
@@ -144,7 +277,7 @@ int main()
                         default:
                             printf("Opción no válida.\n");
                         }
-                    } while (opcionMultas != 2);
+                    } while (opcionMultas != 2 && tieneMultas(usuarioActual));
                 }
                 else
                 {
@@ -161,11 +294,9 @@ int main()
                         {
                         case 1:
                             solicitarPrestamo(usuarioActual);
-                            usuarioActual->tienePrestamo = 1;
                             break;
                         case 2:
                             devolverLibro(usuarioActual);
-                            usuarioActual->tienePrestamo = 0;
                             break;
                         case 3:
                             printf("Saliendo del menú de usuario.\n");
